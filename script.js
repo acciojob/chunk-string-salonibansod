1ad031116061ca5d4d785b87c4102baf3d943d7c
salonibansod <147966433+salonibansod@users.noreply.github.com>
2025-02-04 12:52:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function stringChop(str, size) {
-  // your code here
+	if (!str || size <= 0) return [];
+
+  const chunks = [];
+
+  for (let i = 0; i < str.length; i += size) {
+    chunks.push(str.substring(i, i + size));
+  }
+
+  return chunks;
 }
+
+  // your code here
+
 
 // Do not change the code below
 const str = prompt("Enter String.");
